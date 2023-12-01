6994a5c412891cebc60f24ee8cba18bd920b5a26
Женя <esamoilov@yandex.ru>
2023-12-02 01:47:32 +0300

    Гитхабим всю папку

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите ваше имя ");
+
+string username = Console.ReadLine();
+
+ Console.Write ("Привет, ");
+ Console.WriteLine (username);
+ Console.Write (username);
+ Console.Write (", ну ты и дурак ");
